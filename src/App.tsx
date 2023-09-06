021ee48a53031588ebd6d4552315bebb409c6e4e
chihpingkuo <chihpingkuo@msn.com>
2023-09-06 12:23:06 +0800

    add d3.js

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { createSignal } from "solid-js";
 import logo from "./assets/logo.svg";
 import { invoke } from "@tauri-apps/api/tauri";
 import "./App.css";
+import * as d3 from "d3";
 
 function App() {
   const [greetMsg, setGreetMsg] = createSignal("");
@@ -10,6 +11,27 @@ function App() {
   async function greet() {
     // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
     setGreetMsg(await invoke("greet", { name: name() }));
+  }
+  function LinePlot({
+    data = [1, 2, 3, 4],
+    width = 640,
+    height = 400,
+    marginTop = 20,
+    marginRight = 20,
+    marginBottom = 20,
+    marginLeft = 20
+  }) {
+    const x = d3.scaleLinear([0, data.length - 1], [marginLeft, width - marginRight]);
+    const y = d3.scaleLinear(d3.extent(data), [height - marginBottom, marginTop]);
+    const line = d3.line((d, i) => x(i), y);
+    return (
+      <svg width={width} height={height}>
+        <path fill="none" stroke="currentColor" stroke-width="1.5" d={line(data)} />
+        <g fill="white" stroke="currentColor" stroke-width="1.5">
+          {data.map((d, i) => (<circle key={i} cx={x(i)} cy={y(d)} r="2.5" />))}
+        </g>
+      </svg>
+    );
   }
 
   return (
@@ -46,6 +68,7 @@ function App() {
       </form>
 
       <p>{greetMsg()}</p>
+      <LinePlot/>
     </div>
   );
 }
