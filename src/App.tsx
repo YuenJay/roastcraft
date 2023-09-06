7f7c3fd1fe91794a1ff8dc3abfca8b4c5032e1ec
chihpingkuo <chihpingkuo@msn.com>
2023-09-06 15:55:41 +0800

    fix typescript errors

--- src/App.tsx
+++ src/App.tsx
@@ -22,13 +22,13 @@ function App() {
     marginLeft = 20
   }) {
     const x = d3.scaleLinear([0, data.length - 1], [marginLeft, width - marginRight]);
-    const y = d3.scaleLinear(d3.extent(data), [height - marginBottom, marginTop]);
-    const line = d3.line((d, i) => x(i), y);
+    const y = d3.scaleLinear(d3.extent(data) as [number, number], [height - marginBottom, marginTop]);
+    const line = d3.line((_d, i) => x(i), y);
     return (
       <svg width={width} height={height}>
-        <path fill="none" stroke="currentColor" stroke-width="1.5" d={line(data)} />
+        <path fill="none" stroke="currentColor" stroke-width="1.5" d={line(data) as string | undefined} />
         <g fill="white" stroke="currentColor" stroke-width="1.5">
-          {data.map((d, i) => (<circle key={i} cx={x(i)} cy={y(d)} r="2.5" />))}
+          {data.map((d, i) => (<circle cx={x(i)} cy={y(d)} r="2.5" />))}
         </g>
       </svg>
     );
