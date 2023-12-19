3d235d3219a3ed0f02859b395333b81b7ca2ffc0
chihpingkuo <chihpingkuo@msn.com>
2023-12-19 13:15:48 +0800

    load open file

--- src/fileUtil.ts
+++ src/fileUtil.ts
@@ -3,40 +3,51 @@ import { readTextFile, writeTextFile } from "@tauri-apps/api/fs";
 import { GET, SET, AppStatus, EventId, Point, RoastPhase, appStateSig } from "./AppState";
 
 export async function openFile() {
+    const [appState, _setAppState] = appStateSig;
+
     try {
         let filepath = await open() as string;
         let content = await readTextFile(filepath);
 
+        let loadObject = JSON.parse(content);
+        loadObject.channelList.forEach((c: any) => {
+            let channel = appState().channelListSig[GET]().find((channel) => channel.id == c.id);
+            if (channel) {
+                c.data.forEach((p: Point) => {
+                    channel?.setData([...channel.data(), p]);
+                });
+            }
+        });
     } catch (e) {
         console.log(e);
     }
 }
 
 export async function saveFile() {
-    const [appState, setAppState] = appStateSig;
+    const [appState, _setAppState] = appStateSig;
     try {
         let filepath = await save() as string;
         if (!filepath) return;
 
-        let save_object = {
+        let saveObject = {
             channelList: new Array<any>()
         };
 
-        appState().channelListSig[GET]().forEach((c) => {
+        appState().channelListSig[GET]().forEach((channel) => {
 
-            let data = new Array<Point>();
+            let saveData = new Array<Point>();
 
-            c.data().forEach((p) => {
-                data.push(p)
+            channel.data().forEach((p) => {
+                saveData.push(p)
             });
 
-            save_object.channelList.push({
-                id: c.id,
-                data: data
+            saveObject.channelList.push({
+                id: channel.id,
+                data: saveData
             });
         });
 
-        await writeTextFile(filepath, JSON.stringify(save_object, null, 2));
+        await writeTextFile(filepath, JSON.stringify(saveObject, null, 2));
 
     } catch (e) {
         console.log(e);
