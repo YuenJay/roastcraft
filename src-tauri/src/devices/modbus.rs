a6a46fb14ef13bd68c6af1b9cd19e1dbf5293d14
chihpingkuo <chihpingkuo@msn.com>
2023-12-25 13:40:41 +0800

    use rmodbus

--- src-tauri/src/devices/modbus.rs
+++ src-tauri/src/devices/modbus.rs
@@ -2,21 +2,20 @@
 
 use async_trait::async_trait;
 use log::{debug, error, trace};
+use rmodbus::{client::ModbusRequest, guess_response_frame_len, ModbusProto};
 use serde_json::{to_value, Map, Value};
-use std::io::{Error, ErrorKind};
-use tokio::time;
-use tokio_modbus::{
-    client::{rtu, Context},
-    prelude::Reader,
-    slave::SlaveContext,
-    Slave,
+use serialport::{DataBits, Parity, SerialPort, StopBits};
+use std::{
+    io::{Error, ErrorKind},
+    time::Duration,
 };
+use tokio::time;
 
 use super::Device;
 use crate::config::Config;
 
 pub struct ModbusDevice {
-    ctx: Context,
+    stream: Box<dyn SerialPort>,
     config: Config,
 }
 
@@ -24,39 +23,40 @@ impl ModbusDevice {
     pub fn new(config: Config) -> ModbusDevice {
         let serial = config.serial.as_ref().unwrap();
 
-        let mut builder = tokio_serial::new(serial.port.clone(), serial.baud_rate as u32);
+        // let mut builder = tokio_serial::new(serial.port.clone(), serial.baud_rate as u32);
+        let timeout = Duration::from_secs(1);
 
-        if serial.data_bits == 8 {
-            builder = builder.data_bits(tokio_serial::DataBits::Eight);
-        } else if serial.data_bits == 7 {
-            builder = builder.data_bits(tokio_serial::DataBits::Seven);
+        let mut data_bits = DataBits::Eight;
+        if serial.data_bits == 7 {
+            data_bits = DataBits::Seven;
         } else if serial.data_bits == 6 {
-            builder = builder.data_bits(tokio_serial::DataBits::Six);
+            data_bits = DataBits::Six;
         } else if serial.data_bits == 5 {
-            builder = builder.data_bits(tokio_serial::DataBits::Five);
+            data_bits = DataBits::Five;
         }
 
+        let mut parity = Parity::None;
         let parity_lowercase = serial.parity.to_lowercase();
-        if parity_lowercase == "none" {
-            builder = builder.parity(tokio_serial::Parity::None);
-        } else if parity_lowercase == "even" {
-            builder = builder.parity(tokio_serial::Parity::Even);
+        if parity_lowercase == "even" {
+            parity = Parity::Even;
         } else if parity_lowercase == "odd" {
-            builder = builder.parity(tokio_serial::Parity::Odd);
+            parity = Parity::Odd;
         }
 
-        if serial.stop_bits == 1 {
-            builder = builder.stop_bits(tokio_serial::StopBits::One)
-        } else if serial.stop_bits == 2 {
-            builder = builder.stop_bits(tokio_serial::StopBits::Two)
+        let mut stop_bits = StopBits::One;
+        if serial.stop_bits == 2 {
+            stop_bits = StopBits::Two;
         }
 
-        debug!("{:?}", builder);
+        let mut stream = serialport::new(serial.port.clone(), serial.baud_rate as u32)
+            .data_bits(data_bits)
+            .parity(parity)
+            .stop_bits(stop_bits)
+            .timeout(timeout)
+            .open()
+            .expect("Failed to open port");
 
-        let port = tokio_serial::SerialStream::open(&builder).unwrap();
-        let ctx = rtu::attach(port);
-
-        ModbusDevice { ctx, config }
+        ModbusDevice { stream, config }
     }
 }
 
@@ -74,29 +74,42 @@ impl Device for ModbusDevice {
             let slaves = &modbus.slave;
 
             for slave in slaves {
-                self.ctx.set_slave(Slave(slave.id as u8));
+                // create request object
+                let mut mreq = ModbusRequest::new(slave.id as u8, ModbusProto::Rtu);
+                let mut request = Vec::new();
 
-                // only support function = 3 (read holding register)
-                match self
-                    .ctx
-                    .read_holding_registers(slave.registry as u16, 1)
-                    .await
-                {
-                    Ok(v) => {
-                        let result = *v.get(0).unwrap() as f64 / slave.divisor as f64;
-                        let rounded_number = (result * 10.0).round() / 10.0;
+                // get holding registers
+                mreq.generate_get_holdings(slave.registry, 1, &mut request)
+                    .unwrap();
 
-                        // trace!("{} : {}", slave.channel_id, rounded_number);
-
-                        map.insert(
-                            slave.channel_id.clone(),
-                            to_value(rounded_number).expect("Conversion failed"),
-                        );
-                    }
-                    Err(_) => {
-                        error!("read_holding_registers failed");
-                    }
+                self.stream.write(&request).unwrap();
+                let mut buf = [0u8; 6];
+                self.stream.read_exact(&mut buf).unwrap();
+                let mut response = Vec::new();
+                response.extend_from_slice(&buf);
+                let len = guess_response_frame_len(&buf, ModbusProto::Rtu).unwrap();
+                if len > 6 {
+                    let mut rest = vec![0u8; (len - 6) as usize];
+                    self.stream.read_exact(&mut rest).unwrap();
+                    response.extend(rest);
                 }
+
+                let mut data = Vec::new();
+
+                // check if frame has no Modbus error inside and parse response bools into data vec
+                mreq.parse_u16(&response, &mut data).unwrap();
+                for i in 0..data.len() {
+                    println!("{} {}", i, data[i]);
+                }
+
+                let rounded_number = (data[0] as f64 * 10.0).round() / 100.0;
+
+                trace!("{} : {}", slave.channel_id, rounded_number);
+
+                map.insert(
+                    slave.channel_id.clone(),
+                    to_value(rounded_number).expect("Conversion failed"),
+                );
             }
         });
 
@@ -110,7 +123,7 @@ impl Device for ModbusDevice {
                 ));
             }
         }
-        // trace!("result map : {:?} ", map);
+        trace!("result map : {:?} ", map);
         Ok(Value::Object(map))
     }
 }
