ef558f8c96234d70d37dfaedc0171182fca7675f
chihpingkuo <chihpingkuo@msn.com>
2024-01-18 16:27:06 +0800

    flavor wheel checkbox

--- src/NotesPanel.tsx
+++ src/NotesPanel.tsx
@@ -9,6 +9,58 @@ export default function NotesPanel(props: any) {
 
     const [recentTitles, setRecentTitles] = createSignal(JSON.parse(localStorage.getItem("recentTitles") || "[]") as Array<string>);
 
+    let flavor = [
+        {
+            name: "Sour/Fermented", checked: false,
+            children: [
+                {
+                    name: "Sour", checked: false,
+                    children: [
+                        { name: "Sour Aromatics", checked: false, },
+                        { name: "Acetic Acid", checked: false, },
+                        { name: "Butyric Acid", checked: false, },
+                        { name: "Isovaleric Acid", checked: false, },
+                        { name: "Citric Acid", checked: false, },
+                        { name: "Malic Acid", checked: false, },
+                    ]
+                },
+                {
+                    name: "Alcohol/Fermented",
+                    checked: false,
+                    children: [
+                        { name: "Winey", checked: false, },
+                        { name: "Whiskey", checked: false, },
+                        { name: "Fermented", checked: false, },
+                        { name: "Overripe", checked: false, },
+                    ]
+                }
+            ]
+        },
+        {
+            name: "Fruity", checked: false,
+            children: [
+                {
+                    name: "Berry", checked: false,
+                    children: [
+                        { name: "Blackberry", checked: false, },
+                        { name: "Raspberry", checked: false, },
+                        { name: "Blueberry", checked: false, },
+                        { name: "Strawberry", checked: false, },
+                    ]
+                },
+                {
+                    name: "Dried Fruit",
+                    checked: false,
+                    children: [
+                        { name: "Raisin", checked: false, },
+                        { name: "Prune", checked: false, },
+
+                    ]
+                }
+            ]
+        }
+    ]
+
     createEffect(() => {
 
     });
@@ -108,6 +160,49 @@ export default function NotesPanel(props: any) {
             <textarea class="textarea textarea-bordered textarea-xs w-full h-36" placeholder="Roast Notes"></textarea>
 
             <textarea class="textarea textarea-bordered textarea-xs w-full h-36" placeholder="Cupping Notes"></textarea>
+
+            <For each={flavor}>
+                {(f) => (
+                    <>
+                        <details open={false}>
+                            <summary class="space-x-1">
+                                <input type="checkbox" />
+                                <span>{f.name}</span>
+                            </summary>
+                            <div class="pl-4">
+                                <For each={f.children}>
+                                    {(ff) => (
+                                        <>
+                                            <details >
+                                                <summary class="space-x-1">
+                                                    <input type="checkbox" />
+                                                    <span>{ff.name}</span>
+                                                </summary>
+                                                <div class="pl-4">
+                                                    <For each={ff.children}>
+                                                        {(fff) => (
+                                                            <>
+                                                                <div class="pl-4 space-x-1">
+                                                                    <input type="checkbox" />
+                                                                    <span>{fff.name}</span>
+                                                                </div>
+                                                            </>
+                                                        )}
+                                                    </For>
+                                                </div>
+                                            </details>
+                                        </>
+                                    )}
+                                </For>
+                            </div>
+                        </details>
+                    </>
+                )}
+            </For>
+
+
+
+
 
         </div>
     )
